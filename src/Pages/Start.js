c03c0e0d32f943332ee9ebd95f3ed40d51c1cd54
roxanne56 <elena.roxana.moraru@gmail.com>
2022-12-07 14:59:50 +0200

    add project

---
+++ src/Pages/Start.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+const Start = () => {
+  return (
+    <div
+      className="bkg"
+      style={{
+        backgroundImage: `url("https://images7.alphacoders.com/106/1062176.jpg")`,
+        height: "100vh",
+
+        marginTop: "-70px",
+        fontSize: "50px",
+        backgroundSize: " cover",
+
+        backgroundRepeat: "no-repeat",
+      }}
+    ></div>
+  );
+};
+
+export default Start;
